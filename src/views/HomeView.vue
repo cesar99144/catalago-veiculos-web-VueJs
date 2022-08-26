bc459981994026f1bc64f97c59d56eee393da0e7
César Ferreira <cesarferreiraa67@gmail.com>
2022-08-25 22:07:03 -0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Header />
+    <Destaques />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  import Header from '@/components/Header.vue'
+  import Veiculos from '@/components/Veiculos.vue'
+  import Destaques from '@/components/Destaques.vue';
+
+  export default {
+    name: 'HomeView',
+    components:{
+      Veiculos,
+      Header,
+      Destaques
+    }
   }
-}
 </script>
+
+<style>
+  *{
+    margin: 0px;
+  }
+
+  body{
+
+      background: #F1F1F1;
+  }
+</style>
